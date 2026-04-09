d2de0f1976beeef8bf5113157f3de3a811a263d1
Aman Gupta <35216109+amangupta0105@users.noreply.github.com>
2026-04-09 14:53:39 +0530

    Revert "Fix/backend/auth"

--- server/src/db.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const MONGO_URI = process.env.MONGO_URI;
-const mongoose = require('mongoose');
-
-const connectDb = mongoose.connect(MONGO_URI).then(()=>console.log('Database successfully connected')
-).catch((err)=>console.log(`Error in connecting database`,err));
-
-module.exports ={connectDb}
